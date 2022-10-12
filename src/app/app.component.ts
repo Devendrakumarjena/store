cc8f9cb5a53aca0b17f76790c541536dace3aa1e
devendrakumarjena <devendrajenatrichy@gmail.com>
2022-10-13 00:06:04 +0530

    Addition of angular material & tailwing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    testing
+  <button mat-button color="primary">Primary</button>
     <router-outlet></router-outlet>
   `,
   styles: []
